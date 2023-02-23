c7842b08ab18ad0890543ae31fdc6379803815e5
Michał Futera <100314711+mjfutera@users.noreply.github.com>
2023-02-23 13:44:30 +0100

    v. 1.001

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // ZivahowyServer Wordpress template
-// v. 1.000
+// v. 1.001
 // By Michal Futera
 // http://linktr.ee/mjfutera
 
@@ -13,3 +13,22 @@ const copyToClipboard = fieldName => {
         console.error("Błąd kopiowania do schowka: ", error);
       });
 }
+
+const getData = async (url) => await fetch(url).then(r => r.json());
+
+const serverData = async () => {
+  const data = await getData("https://mcapi.us/server/status?ip=147.135.160.192&port=25598");
+  document.getElementById("subtitle").innerText = data['motd'];
+  document.getElementById("serverStatus").innerText = data['online'] ? "online" : "offline";
+  document.getElementById("serverMaxPlayers").innerText = data['players']['max'];
+  document.getElementById("serverCurrentPlayers").innerText = data['players']['now'];
+  if(data['players']['now']>0){
+    document.getElementById("serverCurrentPlayerList").innerText = data['players']['sample'].map(e=>e['name']).join(', ');
+
+  }
+  setTimeout("serverData", 30000);
+}
+
+window.onload = () => {
+  serverData();
+};
